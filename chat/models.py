f3cc1854a31f349b987eb581c555290d09899468
bibaswanroy <bibaswan.sps19@gmail.com>
2024-03-11 22:57:30 +0530

    naya changes

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=100000)
+
+class Message(models.Model):
+    username = models.CharField(max_length=100000)
+    date = models.DateTimeField(default=datetime.now, blank=True)
+    room = models.CharField(max_length=100000)
+    value = models.CharField(max_length=1000000)
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Room, Message
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def checkview(request):
+    room = request.POST['room_name']
+    username = request.POST['username']
+    if Room.objects.filter(name=room).exists():
+        return redirect('/' + room + '/?username=' + username)
+    else:
+        new_room = Room.objects.create(name=room)
+        new_room.save()
+        return redirect('/' + room + '/?username=' + username)
+
+def room(request, room):
+    room_details = Room.objects.get(name=room)
+    username = request.GET['username']
+    return render(request, 'room.html',
+                {'username':username,
+                 'room':room,
+                 'room_details':room_details})
