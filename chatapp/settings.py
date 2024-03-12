f4390d3e6cb9f26bbfda2de0b97bf783a6fafeaf
bibaswanroy <bibaswan.sps19@gmail.com>
2024-03-12 13:47:58 +0530

    nice

--- chatapp/settings.py
+++ chatapp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5byy$xqul0fvsc#6bsqdpt8a+ohc(kk&xvx-xm49sj0!f3gmp%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '1ad8-223-223-153-104.ngrok-free.app']
 
 
 # Application definition
@@ -122,3 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = ['https://1ad8-223-223-153-104.ngrok-free.app']
