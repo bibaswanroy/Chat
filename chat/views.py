a176d9865c2ffccc26c7cf1df3a020ab499d5bcc
bibaswanroy <bibaswan.sps19@gmail.com>
2024-03-27 01:40:40 +0530

    Empty messages handled

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from .models import Room, Message
 from django.http import JsonResponse, HttpResponse
+from django.contrib import messages
 
 # Create your views here.
 
@@ -35,7 +36,9 @@ def sendMessages(request):
     room_id = request.POST['room_id']
     username = request.POST['username']
     message = request.POST['message']
-
+    # if message == '':
+    #     return render(request, 'room.html',{'alert':"Message body cannot be empty"})
+    
     new_message = Message.objects.create(username=username,room=room_id,value=message)
     new_message.save()
 
